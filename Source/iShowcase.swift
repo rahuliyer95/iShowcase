bd7c3089972e5c7a4c208331dc395975d3cde7e4
rahuliyer95 <rahuliyer573@gmail.com>
2017-05-27 15:14:52 +0530

    Fix #16: Xcode 8.3.2 warnings resolved

--- Source/iShowcase.swift
+++ Source/iShowcase.swift
@@ -306,15 +306,15 @@ import Foundation
                 context?.setShadow(offset: CGSize.zero, blur: 30, color: highlightColor.cgColor)
                 context?.setFillColor(coverColor.cgColor)
                 context?.setStrokeColor(highlightColor.cgColor)
-                context?.addArc(center: center, radius: CGFloat(radius * 2), startAngle: 0, endAngle: CGFloat(2 * M_PI), clockwise: false)
+                context?.addArc(center: center, radius: CGFloat(radius * 2), startAngle: 0, endAngle: CGFloat(2 * Double.pi), clockwise: false)
                 context?.drawPath(using: .fillStroke)
-                context?.addArc(center: center, radius: CGFloat(radius), startAngle: 0, endAngle: CGFloat(2 * M_PI), clockwise: false)
+                context?.addArc(center: center, radius: CGFloat(radius), startAngle: 0, endAngle: CGFloat(2 * Double.pi), clockwise: false)
                 context?.drawPath(using: .fillStroke)
 
                 // Clear circle
                 context?.setFillColor(UIColor.clear.cgColor)
                 context?.setBlendMode(.clear)
-                context?.addArc(center: center, radius: CGFloat(radius - 0.54), startAngle: 0, endAngle: CGFloat(2 * M_PI), clockwise: false)
+                context?.addArc(center: center, radius: CGFloat(radius - 0.54), startAngle: 0, endAngle: CGFloat(2 * Double.pi), clockwise: false)
                 context?.drawPath(using: .fill)
                 context?.setBlendMode(.normal)
             }
